b17971449bd1a0f06a58645c1dc68e4541b2bee2
Scarlett Solórzano <ssolorzano0@ucol.mx>
2025-07-07 22:05:11 -0600

    Completed loops exercises

---
+++ Loops/For/SecondExercise.java
@@ -0,0 +1,10 @@
+public class SecondExercise {
+    
+    public static void main(String[] args) {
+        
+
+        for(int i=1;  i<=22; i++){
+            System.out.println( i);
+        }
+    }
+}
